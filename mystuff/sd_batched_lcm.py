55bfb63b18041cb199d67fb61b16cb2cfddc5f23
Alan Yuan <alan.yuan.jly@gmail.com>
2024-03-19 23:39:12 -0400

    found better model

--- mystuff/sd_batched_lcm.py
+++ mystuff/sd_batched_lcm.py
@@ -1,19 +1,23 @@
-from diffusers import DiffusionPipeline
-from diffusers.pipelines.latent_consistency_models.pipeline_latent_consistency_text2img import LatentConsistencyModelPipeline
+from diffusers import DiffusionPipeline, UNet2DConditionModel, LCMScheduler
 import torch
 
-pipe = LatentConsistencyModelPipeline.from_pretrained("SimianLuo/LCM_Dreamshaper_v7")
+unet = UNet2DConditionModel.from_pretrained(
+    "latent-consistency/lcm-sdxl",
+    torch_dtype=torch.float16,
+    variant="fp16",
+)
+pipe = DiffusionPipeline.from_pretrained(
+    "stabilityai/stable-diffusion-xl-base-1.0", unet=unet, torch_dtype=torch.float16
+).to("cuda")
 
-# To save GPU memory, torch.float16 can be used, but it may compromise image quality.
-pipe.to(torch_device="cuda", torch_dtype=torch.float16)
-print(type(pipe))
+pipe.scheduler = LCMScheduler.from_config(pipe.scheduler.config)
 
 prompt = "Self-portrait oil painting, a beautiful cyborg with golden hair, 8k"
 
-# Can be set to 1~50 steps. LCM support fast inference even <= 4 steps. Recommend: 1~8 steps.
-num_inference_steps = 4 
-
-images = pipe(prompt=prompt, num_inference_steps=num_inference_steps, guidance_scale=8.0, lcm_origin_steps=4, output_type="pil", num_images_per_prompt=8).images
+generator = torch.manual_seed(0)
+images = pipe(
+    prompt=prompt, num_inference_steps=4, generator=generator, guidance_scale=8.0
+).images
 
 counter = 0
 
